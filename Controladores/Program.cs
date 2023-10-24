4f3ee2e46c2f8f6a7b17ed3e9df41a0c4be535c0
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2023-10-24 13:39:18 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace NumeroCuadrado.Controladores;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        MenuInterfaz menu = new MenuImplementacion();
+        menu.mostrarMensajeBienvenida();
+
+        bool cerrarMenu = false;
+        int opcionIntroducida;
+        while(!cerrarMenu)
+        {
+            int n1;
+            int numeroEntero;
+            double numeroEntero2;
+            opcionIntroducida = menu.mostrarMenuYSeleccion();
+            switch (opcionIntroducida)
+            {
+                case 0:
+                    cerrarMenu = true;
+                    break;
+                case 1:
+                    Console.WriteLine("Introduzca un numero entero");
+                    numeroEntero = Int32.Parse(Console.ReadLine());
+                    numeroEntero2 = Math.Sqrt(numeroEntero);
+                    Console.WriteLine(numeroEntero2);
+                    n1 = Convert.ToInt32(numeroEntero2);
+                    
+                    if(n1 * n1 == numeroEntero)
+                    {
+                     Console.WriteLine("Es un numero entero");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un numero entero");
+                    }
+
+                    break;
+
+                   
+            }
+
+
+                   
+                    
+
+                    
+
+            }
+        }
+    }
+
---
+++ MenuImplementacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroCuadrado
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void mostrarMensajeBienvenida()
+        {
+            string mensaje = "NumeroEntero";
+            Console.WriteLine(mensaje);
+        }
+
+        public int mostrarMenuYSeleccion()
+        {
+            int opcionIntroducida;
+            Console.WriteLine("0. cerrar");
+            Console.WriteLine("1. ejecutar");
+            opcionIntroducida = Console.ReadKey(true).KeyChar - '0';
+            return opcionIntroducida;
+        }
+    }
+}
